398c9e48e035d4793adf488b359e146aee2f6551
Chetan Verma <chetan61verma@gmail.com>
2020-05-28 01:27:32 +0530

    Changes

--- src/components/summary.js
+++ src/components/summary.js
@@ -11,14 +11,10 @@ const Summary = () => {
   let tl = new TimelineLite()
   useEffect(() => {
     if (inView) {
-      tl.from(".heading_inner", 1, {
-        y: 350,
-        ease: Power3.easeOut,
-      })
-        .from(".heading p", 0.8, { opacity: 0, y: 20, ease: Power3.easeOut })
+      tl.from(".heading p", 0.8, { opacity: 0, y: 20, ease: Power3.easeOut })
         .from(".summary_video_wrapper", 1, {
           opacity: 0,
-          y: 200,
+          x: 200,
           ease: Power3.easeOut,
         })
         .from(
@@ -38,13 +34,15 @@ const Summary = () => {
     <div className="container bg_white">
       <div className="summary_wrapper" ref={ref}>
         <div className="heading">
-          <h1>
-            <div className="heading_inner">Why GokenGo.</div>
-          </h1>
-          <p>
-            GokenGo is a 24 hour operations monitoring that enables huge savings
-            powered by the inbuilt shift handover process.
+          <p className="head_text">
+            GokenGo is a 24 hour operations monitoring platform that enables
+            huge savings powered by the built-in shift handover process.
           </p>
+          <p>
+            Technology enabled by the internet and smartphones are ubiquitous in
+            our personal lives. Why not use technology to empower us at works ?
+          </p>
+          <button className="learn_how_btn">Learn How</button>
         </div>
         <div className="summary_video_wrapper">
           <video autoPlay loop muted>
@@ -52,11 +50,7 @@ const Summary = () => {
             Your browser does not support the video tag.
           </video>
         </div>
-        <p className="below_video_text">
-          Technology enabled by the internet and smartphones are ubiquitous in
-          our personal lives. Why not use technology to empower us at works ?
-        </p>
-        <button className="learn_how_btn">Learn How</button>
+        >
       </div>
     </div>
   )
